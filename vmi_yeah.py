4ccf98d66f69ad9387a26229a9ad7d21adf04351
SchrankSebestyen <schranksebestyen@gmail.com>
2026-05-11 09:20:42 +0200

    kesz van

--- vmi_yeah.py
+++ vmi_yeah.py
@@ -1,11 +1,15 @@
 import turtle as t
-t.speed(1)
+t.speed(0)
+t.color("red")
 t.width(3)
-
 #haz alja
 i=0
 while i<4:
     t.forward(100)
     t.right(90)
     i+=1
+t.setheading(55)
+t.forward(90)
+t.setheading(-55)
+t.forward(90)
 t.done()
